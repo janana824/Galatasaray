7c49c8fed29d0fd413ba18b0689515036c10b967
janana824 <janana824@gmail.com>
2025-11-10 20:29:44 -0600

    Actualización

--- Galatasaray/Views/MainPage.xaml.cs
+++ Galatasaray/Views/MainPage.xaml.cs
@@ -7,23 +7,44 @@ namespace Galatasaray
     public partial class MainPage : ContentPage
     {
         private readonly SensorViewModel vm;
+
         public MainPage(SensorViewModel vm)
         {
             InitializeComponent();
             this.vm = vm;
             BindingContext = vm;
+
+            // Detectar cambios de orientación
+            SizeChanged += OnSizeChanged;
         }
 
         protected override void OnAppearing()
         {
             base.OnAppearing();
             vm.OnAppearing();
+
+            // Verificar orientación al aparecer la página
+            UpdateOrientation();
         }
 
         protected override void OnDisappearing()
         {
             base.OnDisappearing();
             vm.OnDisappearing();
+        }
+
+        private void OnSizeChanged(object sender, EventArgs e)
+        {
+            UpdateOrientation();
+        }
+
+        private void UpdateOrientation()
+        {
+            bool isLandscape = Width > Height;
+
+            // Cambiar visibilidad de los layouts según orientación
+            PortraitLayout.IsVisible = !isLandscape;
+            LandscapeLayout.IsVisible = isLandscape;
         }
     }
 }
